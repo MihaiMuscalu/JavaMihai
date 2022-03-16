56d36262fa03ba30dcb747d014d0087e887c3dc5
Mihai <mihai.muscalu@student.htldornbirn.at>
2022-03-16 15:31:40 +0100

    added main file

---
+++ src/Bankomat.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Bankomat {
+
+    public static void main(String[] args) {
+
+        boolean end = false;
+        while (!end)
+        {
+        int Balance = 0;
+        Scanner scanner = new Scanner(System.in);
+
+
+
+            System.out.println("1. Einzahlen");
+            System.out.println("2. Abnehmen");
+            System.out.println("3. Kontostand");
+            System.out.println("4. Ende");
+
+
+        int selection = scanner.nextInt();
+
+        switch (selection) {
+            case 1:
+                System.out.println("Geben Sie den Betrag ein, den Sie einzahlen möchten:");
+                Balance += scanner.nextInt();
+                System.out.println("Sie haben " + Balance + " Euro eingezahlt!");
+                break;
+            case 2:
+                System.out.println("Wieviel würden sie gerne abnehmen?");
+                Balance -= scanner.nextInt();
+                System.out.println("Sie haben " + Balance + " Euro abgehoben!");
+                break;
+            case 3:
+                System.out.println("Der Kontostand beträgt: " + Balance + " Euro");
+                break;
+            case 4:
+                System.out.println("Machine ist stopping!");
+                end = true;
+                break;
+            }
+        }
+    }
+}
---
+++ src/IF_Aufgabe1.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class IF_Aufgabe1 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(100);
+
+        System.out.println(randomNumber);
+
+        if (randomNumber < 20) {
+            System.out.println("Mini");
+
+        }
+        if (randomNumber > 20 && randomNumber < 50) {
+            System.out.println("Medium");
+
+        }
+        if (randomNumber > 50) {
+            System.out.println("Large");
+
+        }
+
+    }
+}
