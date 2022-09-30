8d57a50f2d1acdda44f0f6324ea323913284122a
Mihai <mihai.muscalu@student.htldornbirn.at>
2022-09-30 14:31:34 +0200

    OO 3

--- src/OO/Main.java
+++ src/OO/Main.java
@@ -1,18 +1,21 @@
 package OO;
+
 import OO.Objects.Car;
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         Car c1 = new Car();
         c1.brand = "Audi";
         c1.fuelConsumption = 7;
         c1.serialNumber = "A1234";
         c1.fuelAmount = 70;
+        c1.Tank = 70;
 
         System.out.println(c1.fuelAmount);
         c1.Drive();
         System.out.println(c1.fuelAmount);
+        c1.TurboBoost();
+        c1.honk(2);
     }
 }
--- src/OO/Objects/Car.java
+++ src/OO/Objects/Car.java
@@ -3,14 +3,55 @@ package OO.Objects;
 public class Car {
 
     public int fuelConsumption;
+    public int Tank;
     public int fuelAmount;
     public String brand;
     public String serialNumber;
     private String color;
 
-    public void Drive()
-    {
-        this.fuelAmount = this.fuelAmount - fuelConsumption;
-        System.out.println("I am driving!");
+    public void Drive() {
+
+        if (fuelAmount >= fuelConsumption) {
+            this.fuelAmount = this.fuelAmount - fuelConsumption;
+            System.out.println("I am driving!");
+        }
+
+    }
+
+    public void Break() {
+
+        System.out.println("I am breaking!");
+
+    }
+
+    public void TurboBoost() {
+
+        if (this.fuelAmount > Tank / 10) {
+            System.out.println("SuperBoostMode!");
+        } else System.out.println("Not enough fuel in tank to go Turbo!");
+
+    }
+
+    public void honk(int Repetitions) {
+
+        while (Repetitions > 0) {
+            System.out.println("tuut");
+        }
+    }
+
+    public void getRemainingRange() {
+
+        int range = fuelAmount / fuelConsumption;
+
+        if (range > 1) {
+            System.out.printf("You can drive %f more kilometers!", range);
+        }
+        if (range == 1) {
+            System.out.printf("You can drive %f more kilometer!", range);
+        }
+        if (range < 1) {
+            System.out.printf("You can't drive any more kilometers!");
+        }
+
     }
 }
