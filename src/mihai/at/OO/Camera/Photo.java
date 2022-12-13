64c418a5ac8c4372fece6ee1f01c79583451f5df
Mihai <mihai.muscalu@student.htldornbirn.at>
2022-12-13 10:38:16 +0100

    Camera basics done!

---
+++ src/mihai/at/OO/Camera/Photo.java
@@ -0,0 +1,24 @@
+package mihai.at.OO.Camera;
+public class Photo {
+    private int size;
+    private String name;
+    private String extension;
+
+    public Photo(int size, String name, String extension) {
+        this.size = size;
+        this.name = name;
+        this.extension = extension;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+}
